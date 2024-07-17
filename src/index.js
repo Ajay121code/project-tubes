1b6cf605de6288cdf8c7a7a76e8272bb4c8651ed
Ajay Tomar <ajaysinghtomer121@gmail.com>
2024-07-17 17:44:24 +0530

    Changes until now

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`\n Server is running on port ${process.env.PORT}`);
+    });
+    console.log("Connected to MongoDB");
+})
+.catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+});
